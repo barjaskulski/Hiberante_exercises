bf82a2232a690c94489b3b4c68e1587e1d99c10c
BJa <barjaskulski@gmail.com>
2021-01-30 19:08:35 +0100

    Hibernate - Practice examples

---
+++ src/main/java/entity/Review.java
@@ -0,0 +1,45 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Review {
+    @Id
+    private Long id;
+    private String content;
+    private int rating;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
+}
